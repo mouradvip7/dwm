d2aba01709a36f25c019157c689f4d67789bab3c
Mourad <mouradvip7@gmail.com>
2021-11-25 14:46:20 +0000

    removed rules

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
+static char *fonts[]          = { "JetBrainsMono:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -85,20 +85,6 @@ static const Rule rules[] = {
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
-	{"Brave",			NULL,				NULL,					1 << 1,					0,						0,					0					-1},
-	{"Ferdi",			NULL,				NULL,					1 << 2,					0,						0,					0					-1},
-	{"Vivaldi",			NULL,				NULL,					1 << 3,					0,						0,					0					-1},
-
-	{"LF",			"lf",				NULL,					1 << 4,					0,						1,					1					-1},
-
-
-	{"Stremio",			"stremio",				NULL,					1 << 5,					0,					0,				0,				-1},
-
-
-
-
-
-
 };
 
 /* layout(s) */
